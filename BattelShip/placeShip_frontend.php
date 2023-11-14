38130515800b17d222bf60b589b797256b0f9f10
MarcoPacchiotti <57013700+pacchio1@users.noreply.github.com>
2023-11-14 14:38:34 +0100

    Updated ship placement UI and added AJAX call for ship positioning

--- BattelShip/placeShip_frontend.php
+++ BattelShip/placeShip_frontend.php
@@ -16,12 +16,15 @@
     </div>
     <br>
     <div>
-        <input type="radio" name="Incrociatore1" value="Incrociatore1">****(Incrociatore)
-        <input type="radio" name="Sottomarino1" value="Sottomarino1">***(Sottomarino 1)
-        <input type="radio" name="Sottomarino2" value="Sottomarino2">***(Sottomarino 2)
-        <input type="radio" name="Cacciatorpediniere1" value="Cacciatorpediniere1">** (Cacciatorpediniere 1)
-        <input type="radio" name="Cacciatorpediniere2" value="Cacciatorpediniere2">** (Cacciatorpediniere 2)
-        <input type="radio" name="Cacciatorpediniere3" value="Cacciatorpediniere3">** (Cacciatorpediniere 3)
+        <input type="radio" name="Incrociatore1" value="Incrociatore1"><img src="img/4.png" alt="4">(Incrociatore)
+        <input type="radio" name="Sottomarino1" value="Sottomarino1"><img src="img/3.png" alt="3">(Sottomarino 1)
+        <input type="radio" name="Sottomarino2" value="Sottomarino2"><img src="img/3.png" alt="3">(Sottomarino 2)
+        <input type="radio" name="Cacciatorpediniere1" value="Cacciatorpediniere1"><img src="img/2.png"
+            alt="2">(Cacciatorpediniere 1)
+        <input type="radio" name="Cacciatorpediniere2" value="Cacciatorpediniere2"><img src="img/2.png"
+            alt="2">(Cacciatorpediniere 2)
+        <input type="radio" name="Cacciatorpediniere3" value="Cacciatorpediniere3"><img src="img/2.png"
+            alt="2">(Cacciatorpediniere 3)
     </div>
 
     <br>
@@ -29,52 +32,52 @@
     <button id="posizionaBarche">Posiziona le barche</button>
 
     <script>
-        function generateGrid() {
-            var table = $('<table>').addClass('blu');
-            for (var i = 0; i < 10; i++) {
-                var row = $('<tr>');
-                for (var j = 0; j < 10; j++) {
-                    (function(x, y) {
-                        var cell = $('<td>').on('click', function() {
-                            var coordinates = x + ',' + y;
-                            console.log('Hai cliccato sulla cella: ' + coordinates);
-                        });
-                        row.append(cell);
-                    })(i, j);
-                }
-                table.append(row);
+    function generateGrid() {
+        var table = $('<table>').addClass('blu');
+        for (var i = 0; i < 10; i++) {
+            var row = $('<tr>');
+            for (var j = 0; j < 10; j++) {
+                (function(x, y) {
+                    var cell = $('<td>').on('click', function() {
+                        var coordinates = x + ',' + y;
+                        console.log('Hai cliccato sulla cella: ' + coordinates);
+                    });
+                    row.append(cell);
+                })(i, j);
             }
-            $('#board').append(table);
+            table.append(row);
         }
+        $('#board').append(table);
+    }
 
-        $('#ruotaBarche').on('click', function() {
+    $('#ruotaBarche').on('click', function() {
 
-        }); //ruota le barche
+    }); //ruota le barche
 
+    //TODO: trasformare posiziona barche in posiziona barca e alla 6 fare la chiamata ajax
+    // Chiamata AJAX
+    $('#posizionaBarche').on('click', function() {
+        var tabella = []; // TODO: inserire la configurazione della griglia con le barche posizionate
+        $.ajax({
+            type: 'POST',
+            url: '../placeShip.php',
+            data: {
+                tabella: tabella
+            },
+            success: function(response) {
+                alert('Barche posizionate con successo!');
 
-        // Chiamata AJAX
-        $('#posizionaBarche').on('click', function() {
-            var tabella = []; // TODO: inserire la configurazione della griglia con le barche posizionate
-            $.ajax({
-                type: 'POST',
-                url: '../placeShip.php',
-                data: {
-                    tabella: tabella
-                },
-                success: function(response) {
-                    alert('Barche posizionate con successo!');
-
-                },
-                error: function() {
-                    alert('Si è verificato un errore durante il posizionamento delle barche.');
-                }
-            });
+            },
+            error: function() {
+                alert('Si è verificato un errore durante il posizionamento delle barche.');
+            }
         });
+    });
 
 
-        $(document).ready(function() {
-            generateGrid();
-        });
+    $(document).ready(function() {
+        generateGrid();
+    });
     </script>
 </body>
 
